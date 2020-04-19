02a3d3f631761cb0eab56ca1b0646aad439ee19b
Piotr Sulej <piotr.krzysztof.sulej@gmail.com>
2020-04-19 23:44:56 +0200

    Remove not-needed access token

--- users/src/main/java/pl/sulej/users/model/network/GitHubUsersAPI.kt
+++ users/src/main/java/pl/sulej/users/model/network/GitHubUsersAPI.kt
@@ -2,16 +2,13 @@ package pl.sulej.users.model.network
 
 import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface GitHubUsersApi {
 
-    @Headers("Authorization: Bearer 5767cc5803c613a77401b2868b759532bc6384ce")
     @GET(value = "users")
     fun getUsers(): Single<List<UserDto>>
 
-    @Headers("Authorization: Bearer 5767cc5803c613a77401b2868b759532bc6384ce")
     @GET(value = "users/{login}/repos")
     fun getUserRepositories(@Path("login") userLogin: String): Single<List<RepositoryDto>>
 }
